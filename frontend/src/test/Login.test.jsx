7f50dea1236bcd1c701820300bedb8f7e6247105
ANKITA <ankitaakaliaa.21@gmail.com>
2026-09-11 18:35:45 +1000

    Temporarily break a frontend test to demonstrate CI gate

--- frontend/src/test/Login.test.jsx
+++ frontend/src/test/Login.test.jsx
@@ -40,7 +40,7 @@ describe("Login page", () => {
 
     expect(
       screen.getByText(
-        "KoalaTech University"
+        "This Text Does Not Exist On The Page"
       )
     ).toBeInTheDocument();
 
