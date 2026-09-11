c8b0f310567d3e6e0f5190c0df7f94a84f8bf987
ANKITA <ankitaakaliaa.21@gmail.com>
2026-09-11 18:38:41 +1000

    Revert deliberate test failure

--- frontend/src/test/Login.test.jsx
+++ frontend/src/test/Login.test.jsx
@@ -40,7 +40,7 @@ describe("Login page", () => {
 
     expect(
       screen.getByText(
-        "This Text Does Not Exist On The Page"
+        "KoalaTech University"
       )
     ).toBeInTheDocument();
 
